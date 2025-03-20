6c8d55f6cf2541df72b23fb38a227368199fbf07
OmarSalah885 <omarsalah00013@gmail.com>
2025-03-20 16:51:25 +0300

    separate the code to header and nav and footer and make the categories dynamic

---
+++ header.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+include 'db_connect.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="css/main.css" rel="stylesheet">
+      <link href="https://fonts.googleapis.com" rel="preconnect">
+      <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+      <link
+         href="https://fonts.googleapis.com/css2?family=Lexend+Tera:wght@100..900&display=swap"
+         rel="stylesheet">
+      <!-- Font Awesome Free CDN -->
+      <link
+         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+         rel="stylesheet">
+      <title>Taqeem</title>
+   </head>
+   <body>
