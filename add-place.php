e8497fb193f693033c8c4eb60b30c1f0ef1c12c2
abd-ulrhman-20 <abdultraman@gmail.com>
2025-04-25 20:53:43 +0300

    fixing some add-place styling

--- add-place.php
+++ add-place.php
@@ -31,21 +31,25 @@ include 'header.php';
                 </select>
             </div>
             <textarea name id placeholder="DESCRIPTION ..."></textarea>
-            <select name="CATEGORY" id="CATEGORY" class="input--red">
-                <option value disabled selected class="selected-option">CATEGORY</option>
-                <option value="RESTURANTS">RESTURANTS</option>
-                <option value="SHOPPING">SHOPPING</option>
-                <option value="ACTIVE LIFE">ACTIVE LIFE</option>
-                <option value="HOME SERVICES">HOME SERVICES</option>
-                <option value="COFFEE">COFFEE</option>
-                <option value="PETS">PETS</option>
-                <option value="PLANTS SHOP">PLANTS SHOP</option>
-                <option value="ART">ART</option>
-                <option value="HOTELS">HOTELS</option>
-                <option value="EDUCATION">EDUCATION</option>
-                <option value="HEALTH">HEALTH</option>
-                <option value="WORKSPACE">WORKSPACE</option>
-            </select>
+
+            <div class="side-by-side_inbut">
+                <input type="text" placeholder="TAGS add (,) between the tags" class="input--red">
+                <select name="CATEGORY" id="CATEGORY" class="input--red">
+                    <option value disabled selected class="selected-option">CATEGORY</option>
+                    <option value="RESTURANTS">RESTURANTS</option>
+                    <option value="SHOPPING">SHOPPING</option>
+                    <option value="ACTIVE LIFE">ACTIVE LIFE</option>
+                    <option value="HOME SERVICES">HOME SERVICES</option>
+                    <option value="COFFEE">COFFEE</option>
+                    <option value="PETS">PETS</option>
+                    <option value="PLANTS SHOP">PLANTS SHOP</option>
+                    <option value="ART">ART</option>
+                    <option value="HOTELS">HOTELS</option>
+                    <option value="EDUCATION">EDUCATION</option>
+                    <option value="HEALTH">HEALTH</option>
+                    <option value="WORKSPACE">WORKSPACE</option>
+                </select>
+            </div>
         </div>
         <div class="add-place_main--item add-place_main--highlight " id="add-place-highlight">
             <h2 class="add-place_title">HIGHLIGHTS</h2>
@@ -109,34 +113,62 @@ include 'header.php';
             <input type="url" placeholder="TWITTER URL" class="input--red">
         </div>
         <div class="add-place_main--item add-place_main--opening" id="add-place-opening">
-            <h2 class="add-place_title">SOCIAL NETWORKS</h2>
+            <h2 class="add-place_title">OPENING HOURS</h2>
+
             <div class="side-by-side_inbut">
-                <input type="text" placeholder="Monday" value="Monday" class="input--red">
-                <input type="text" placeholder="Time" value="9:00 AM - 5:00 PM" class="input--red">
+                <input type="text" placeholder="Monday" value="Monday" class="input--red" readonly>
+                <div class="side-by-side_inbut">
+                    <input type="time" class="input--red" value="09:00">
+                    <input type="time" class="input--red" value="17:00">
+                </div>
             </div>
+
             <div class="side-by-side_inbut">
-                <input type="text" placeholder="Tuesday" value="Tuesday" class="input--red">
-                <input type="text" placeholder="Time" value="9:00 AM - 5:00 PM" class="input--red">
+                <input type="text" placeholder="Tuesday" value="Tuesday" class="input--red" readonly>
+                <div class="side-by-side_inbut">
+                    <input type="time" class="input--red" value="09:00">
+                    <input type="time" class="input--red" value="17:00">
+                </div>
             </div>
+
             <div class="side-by-side_inbut">
-                <input type="text" placeholder="Wednesday" value="Wednesday" class="input--red">
-                <input type="text" placeholder="Time" value="9:00 AM - 5:00 PM" class="input--red">
+                <input type="text" placeholder="Wednesday" value="Wednesday" class="input--red" readonly>
+                <div class="side-by-side_inbut">
+                    <input type="time" class="input--red" value="09:00">
+                    <input type="time" class="input--red" value="17:00">
+                </div>
             </div>
+
             <div class="side-by-side_inbut">
-                <input type="text" placeholder="Thursday" value="Thursday" class="input--red">
-                <input type="text" placeholder="Time" value="9:00 AM - 5:00 PM" class="input--red">
+                <input type="text" placeholder="Thursday" value="Thursday" class="input--red" readonly>
+                <div class="side-by-side_inbut">
+                    <input type="time" class="input--red" value="09:00">
+                    <input type="time" class="input--red" value="17:00">
+                </div>
             </div>
+
             <div class="side-by-side_inbut">
-                <input type="text" placeholder="Friday" value="Friday" class="input--red">
-                <input type="text" placeholder="Time" value="9:00 AM - 5:00 PM" class="input--red">
+                <input type="text" placeholder="Friday" value="Friday" class="input--red" readonly>
+                <div class="side-by-side_inbut">
+                    <input type="time" class="input--red" value="09:00">
+                    <input type="time" class="input--red" value="17:00">
+                </div>
             </div>
+
             <div class="side-by-side_inbut">
-                <input type="text" placeholder="Saturday" value="Saturday" class="input--red">
-                <input type="text" placeholder="Time" value="9:00 AM - 5:00 PM" class="input--red">
+                <input type="text" placeholder="Saturday" value="Saturday" class="input--red" readonly>
+                <div class="side-by-side_inbut">
+                    <input type="time" class="input--red" value="09:00">
+                    <input type="time" class="input--red" value="17:00">
+                </div>
             </div>
+
             <div class="side-by-side_inbut">
-                <input type="text" placeholder="Sunday" value="Sunday" class="input--red">
-                <input type="text" placeholder="Time" value="9:00 AM - 5:00 PM" class="input--red">
+                <input type="text" placeholder="Sunday" value="Sunday" class="input--red" readonly>
+                <div class="side-by-side_inbut">
+                    <input type="time" class="input--red" value="09:00">
+                    <input type="time" class="input--red" value="17:00">
+                </div>
             </div>
         </div>
         <div class="add-place_main--item add-place_main--media" id="add-place-media">
@@ -164,10 +196,55 @@ include 'header.php';
                     </div>
                 </div>
             </div>
+            <div class="media_added">
+                <div class="media_added--fetured">
+                    <h3 class="media_added--title">ADDED FETURED IMAGE</h3>
+                    <div class="media_added--fetured_img">
+                        <img src="assets/images/places/restaurants/RM(4).jpg" alt="">
+                        <a href="">X</a>
+                    </div>
+                </div>
+                <div class="media_added--gallery">
+                    <h3 class="media_added--title">ADDED IMAGES FOR GALLERY</h3>
+                    <div class="media_added--gallery_grid">
+                        <div class="media_added--gallery_grid_item">
+                            <img src="assets/images/places/restaurants/R(1).jpg" alt="">
+                            <a href="">X</a>
+                        </div>
+                        <div class="media_added--gallery_grid_item">
+                            <img src="assets/images/places/restaurants/RM(4).jpg" alt="">
+                            <a href="">X</a>
+                        </div>
+                        <div class="media_added--gallery_grid_item">
+                            <img src="assets/images/places/restaurants/RM(4).jpg" alt="">
+                            <a href="">X</a>
+                        </div>
+                        <div class="media_added--gallery_grid_item">
+                            <img src="assets/images/places/restaurants/RM(4).jpg" alt="">
+                            <a href="">X</a>
+                        </div>
+                        <div class="media_added--gallery_grid_item">
+                            <img src="assets/images/places/restaurants/RM(4).jpg" alt="">
+                            <a href="">X</a>
+                        </div>
+                        <div class="media_added--gallery_grid_item">
+                            <img src="assets/images/places/restaurants/RM(4).jpg" alt="">
+                            <a href="">X</a>
+                        </div>
+                        <div class="media_added--gallery_grid_item">
+                            <img src="assets/images/places/restaurants/RM(4).jpg" alt="">
+                            <a href="">X</a>
+                        </div>
+                        <div class="media_added--gallery_grid_item">
+                            <img src="assets/images/places/restaurants/RM(4).jpg" alt="">
+                            <a href="">X</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
         <div class="add-place_main--item add-place_main--menu" id="add-place-menu">
-            <h2 class="add-place_title">MEDIA</h2>
+            <h2 class="add-place_title">MENU</h2>
             <div class="add-menu_item">
                 <div class="add-menu_item--img">
                     <p class="media-contanier_title">MENU ITEM IMAGE</p>
@@ -187,16 +264,86 @@ include 'header.php';
                     <input type="text" placeholder="MENU ITEM DESCRIPTION" class="input--red">
                 </div>
             </div>
-            <button class="btn__red--s btn__red btn">ADD MORE</button>
+            <button class="btn__red--s btn__red btn">ADD ITEM</button>
+            <div class="add-menu_added">
+                <h3>ADDED MENU ITEMS</h3>
+                <div class="add-menu_added-grid">
+                    <div class="add-menu_added-grid_item">
+                        <img src="assets/images/places/restaurants/R(1).jpg" alt="">
+                        <div class="add-menu_added-grid_item--info">
+                            <h4>MENU ITEM NAME</h4>
+                            <p>MENU ITEM DESCRIPTION</p>
+                            <p>MENU ITEM PRICE</p>
+                        </div>
+                        <a href="">X</a>
+                    </div>
+                    <div class="add-menu_added-grid_item">
+                        <img src="assets/images/places/restaurants/R(1).jpg" alt="">
+                        <div class="add-menu_added-grid_item--info">
+                            <h4>MENU ITEM NAME</h4>
+                            <p>MENU ITEM DESCRIPTION</p>
+                            <p>MENU ITEM PRICE</p>
+                        </div>
+                        <a href="">X</a>
+                    </div>
+                    <div class="add-menu_added-grid_item">
+                        <img src="assets/images/places/restaurants/R(1).jpg" alt="">
+                        <div class="add-menu_added-grid_item--info">
+                            <h4>MENU ITEM NAME</h4>
+                            <p>MENU ITEM DESCRIPTION</p>
+                            <p>MENU ITEM PRICE</p>
+                        </div>
+                        <a href="">X</a>
+                    </div>
+                    <div class="add-menu_added-grid_item">
+                        <img src="assets/images/places/restaurants/R(1).jpg" alt="">
+                        <div class="add-menu_added-grid_item--info">
+                            <h4>MENU ITEM NAME</h4>
+                            <p>MENU ITEM DESCRIPTION MENU ITEM DESCRIPTION MENU ITEM DESCRIPTION MENU ITEM DESCRIPTION
+                            </p>
+                            <p>MENU ITEM PRICE</p>
+                        </div>
+                        <a href="">X</a>
+                    </div>
+                    <div class="add-menu_added-grid_item">
+                        <img src="assets/images/places/restaurants/R(1).jpg" alt="">
+                        <div class="add-menu_added-grid_item--info">
+                            <h4>MENU ITEM NAME</h4>
+                            <p>MENU ITEM DESCRIPTION</p>
+                            <p>MENU ITEM PRICE</p>
+                        </div>
+                        <a href="">X</a>
+                    </div>
+                </div>
+            </div>
         </div>
 
         <div class="add-place_main--item add-place_main--faqs" id="add-place-faqs">
             <h2 class="add-place_title">FQAs</h2>
             <input type="text" placeholder="QUESTION" class="input--red">
             <input type="text" placeholder="ANSWER" class="input--red">
-            <button class="btn__red--s btn__red btn">ADD MORE</button>
+            <button class="btn__red--s btn__red btn">ADD QUESTION</button>
+            <div class="added-faqs">
+                <h3>ADDED FAQS</h3>
+                <div class="added-faqs-grid">
+                    <div class="added-faqs-grid_item">
+                        <h4>QUESTION</h4>
+                        <p>ANSWER</p>
+                        <a href="">X</a>
+                    </div>
+                    <div class="added-faqs-grid_item">
+                        <h4>QUESTION</h4>
+                        <p>ANSWER</p>
+                        <a href="">X</a>
+                    </div>
+                    <div class="added-faqs-grid_item">
+                        <h4>QUESTION</h4>
+                        <p>ANSWER</p>
+                        <a href="">X</a>
+                    </div>
+                </div>
+            </div>
         </div>
         <button class="btn__red--l btn__red btn">ADD PLACE !</button>
-    </div>
 </main>
 <?php include 'footer.php'; ?>
--- header.php
+++ header.php
@@ -48,12 +48,14 @@ if (!isset($_SESSION['profile_image']) || !isset($_SESSION['first_name']) || !is
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/main.css" rel="stylesheet">
-    
+
     <link href="https://fonts.googleapis.com" rel="preconnect">
     <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lexend+Tera:wght@100..900&display=swap" rel="stylesheet">
     <!-- Font Awesome Free CDN -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
     <title>Taqeem</title>
     <script>
     const isLoggedIn = <?php echo isset($_SESSION['user_id']) ? 'true' : 'false'; ?>;
@@ -76,21 +78,24 @@ if (!isset($_SESSION['profile_image']) || !isset($_SESSION['first_name']) || !is
                     <a href="index.php"><img src="assets/images/logo.png" alt="logo"></a>
                 </div>
                 <div class="navbar_container--menu-R">
-    <a class="btn__red--m btn__red btn" id="search-btn" href="#"><i class="fa-solid fa-magnifying-glass"></i></a>
-    <?php if (isset($_SESSION['user_id'])): ?>
-        <!-- Show profile link when the user is logged in -->
-        <a href="profile.php?user_id=<?php echo htmlspecialchars($_SESSION['user_id']); ?>" class="navbar_profile">
-    <img src="<?php echo htmlspecialchars($_SESSION['profile_image'] ?? 'assets/images/profiles/pro_null.png') . '?v=' . time(); ?>" alt="User Profile">
-    <span><?php echo htmlspecialchars($_SESSION['first_name'] . ' ' . $_SESSION['last_name']); ?></span>
-        </a>
-        <a class="navbar_container--menu-R_links" href="logout.php">Log Out</a>
-    <?php else: ?>
-        <!-- Show login and signup links when the user is not logged in -->
-        <a class="navbar_container--menu-R_links" id="login-nav" href="#">Log In</a>
-        <a class="navbar_container--menu-R_links" id="signup-nav" href="#">Sign Up</a>
-    <?php endif; ?>
-    <a class="btn__red--m btn__red btn" href="add-place.php">Add Place</a>
-</div>
+                    <a class="btn__red--m btn__red btn" id="search-btn" href="#"><i
+                            class="fa-solid fa-magnifying-glass"></i></a>
+                    <?php if (isset($_SESSION['user_id'])): ?>
+                    <!-- Show profile link when the user is logged in -->
+                    <a href="profile.php?user_id=<?php echo htmlspecialchars($_SESSION['user_id']); ?>"
+                        class="navbar_profile">
+                        <img src="<?php echo htmlspecialchars($_SESSION['profile_image'] ?? 'assets/images/profiles/pro_null.png') . '?v=' . time(); ?>"
+                            alt="User Profile">
+                        <span><?php echo htmlspecialchars($_SESSION['first_name'] . ' ' . $_SESSION['last_name']); ?></span>
+                    </a>
+                    <a class="navbar_container--menu-R_links" href="logout.php">Log Out</a>
+                    <?php else: ?>
+                    <!-- Show login and signup links when the user is not logged in -->
+                    <a class="navbar_container--menu-R_links" id="login-nav" href="#">Log In</a>
+                    <a class="navbar_container--menu-R_links" id="signup-nav" href="#">Sign Up</a>
+                    <?php endif; ?>
+                    <a class="btn__red--m btn__red btn" href="add-place.php">Add Place</a>
+                </div>
 
             </div>
         </nav>
@@ -108,7 +113,8 @@ if (!isset($_SESSION['profile_image']) || !isset($_SESSION['first_name']) || !is
 
         <div class="navbar_search--overlay" id="search-overlay">
             <a id="close-btn" href="#">X</a>
-            <div class="navbar_search--overlay-content"><input type="text" placeholder="search"><button><i class="fa-solid fa-magnifying-glass"></i></button></div>
+            <div class="navbar_search--overlay-content"><input type="text" placeholder="search"><button><i
+                        class="fa-solid fa-magnifying-glass"></i></button></div>
         </div>
 
         <div class="LogOverlay">
@@ -128,15 +134,17 @@ if (!isset($_SESSION['profile_image']) || !isset($_SESSION['first_name']) || !is
                     $login_data = $_SESSION['login_data'] ?? [];
                     unset($_SESSION['login_errors'], $_SESSION['login_data']);
                     ?>
-                    <input type="hidden" name="redirect_url" value="<?php echo htmlspecialchars($_SERVER['REQUEST_URI']); ?>">
-                    <input type="email" name="email" placeholder="EMAIL" value="<?php echo htmlspecialchars($login_data['email'] ?? ''); ?>" required>
+                    <input type="hidden" name="redirect_url"
+                        value="<?php echo htmlspecialchars($_SERVER['REQUEST_URI']); ?>">
+                    <input type="email" name="email" placeholder="EMAIL"
+                        value="<?php echo htmlspecialchars($login_data['email'] ?? ''); ?>" required>
                     <?php if (isset($login_errors['email'])): ?>
-                        <p class="error"><?php echo $login_errors['email']; ?></p>
+                    <p class="error"><?php echo $login_errors['email']; ?></p>
                     <?php endif; ?>
 
                     <input type="password" name="password" placeholder="PASSWORD" required>
                     <?php if (isset($login_errors['password'])): ?>
-                        <p class="error"><?php echo $login_errors['password']; ?></p>
+                    <p class="error"><?php echo $login_errors['password']; ?></p>
                     <?php endif; ?>
 
                     <p>Forgot your password? <a href="#">RESET PASSWORD.</a></p>
@@ -151,34 +159,37 @@ if (!isset($_SESSION['profile_image']) || !isset($_SESSION['first_name']) || !is
                     unset($_SESSION['signup_errors'], $_SESSION['signup_data']);
                     ?>
                     <div class="LogOverlay__content--signup_name">
-                        <input type="text" name="first_name" placeholder="FIRST NAME" value="<?php echo htmlspecialchars($signup_data['first_name'] ?? ''); ?>" required>
+                        <input type="text" name="first_name" placeholder="FIRST NAME"
+                            value="<?php echo htmlspecialchars($signup_data['first_name'] ?? ''); ?>" required>
                         <?php if (isset($signup_errors['first_name'])): ?>
-                            <p class="error"><?php echo $signup_errors['first_name']; ?></p>
+                        <p class="error"><?php echo $signup_errors['first_name']; ?></p>
                         <?php endif; ?>
 
-                        <input type="text" name="last_name" placeholder="LAST NAME" value="<?php echo htmlspecialchars($signup_data['last_name'] ?? ''); ?>" required>
+                        <input type="text" name="last_name" placeholder="LAST NAME"
+                            value="<?php echo htmlspecialchars($signup_data['last_name'] ?? ''); ?>" required>
                         <?php if (isset($signup_errors['last_name'])): ?>
-                            <p class="error"><?php echo $signup_errors['last_name']; ?></p>
+                        <p class="error"><?php echo $signup_errors['last_name']; ?></p>
                         <?php endif; ?>
                     </div>
-                    <input type="email" name="email" placeholder="EMAIL" value="<?php echo htmlspecialchars($signup_data['email'] ?? ''); ?>" required>
+                    <input type="email" name="email" placeholder="EMAIL"
+                        value="<?php echo htmlspecialchars($signup_data['email'] ?? ''); ?>" required>
                     <?php if (isset($signup_errors['email'])): ?>
-                        <p class="error"><?php echo $signup_errors['email']; ?></p>
+                    <p class="error"><?php echo $signup_errors['email']; ?></p>
                     <?php endif; ?>
 
                     <input type="password" name="password" placeholder="PASSWORD" required>
                     <?php if (isset($signup_errors['password'])): ?>
-                        <p class="error"><?php echo $signup_errors['password']; ?></p>
+                    <p class="error"><?php echo $signup_errors['password']; ?></p>
                     <?php endif; ?>
 
                     <input type="password" name="confirm_password" placeholder="CONFIRM PASSWORD" required>
                     <?php if (isset($signup_errors['confirm_password'])): ?>
-                        <p class="error"><?php echo $signup_errors['confirm_password']; ?></p>
+                    <p class="error"><?php echo $signup_errors['confirm_password']; ?></p>
                     <?php endif; ?>
 
                     <button type="submit" class="btn__red--l btn__red btn">Sign up</button>
                     <?php if (isset($signup_errors['general'])): ?>
-                        <p class="error"><?php echo $signup_errors['general']; ?></p>
+                    <p class="error"><?php echo $signup_errors['general']; ?></p>
                     <?php endif; ?>
                 </form>
             </div>
@@ -189,17 +200,18 @@ if (!isset($_SESSION['profile_image']) || !isset($_SESSION['first_name']) || !is
                 <a class="mobile_overlay--content-close" id="mobile_emnu-close" href="#">X</a>
                 <div class="mobile_overlay--content_links">
                     <?php if (isset($_SESSION['user_id'])): ?>
-                        <!-- Display user profile when logged in -->
-                        <div class="mobile_user_profile">
-                            <img src="<?php echo htmlspecialchars($_SESSION['profile_image'] ?? 'assets/images/profiles/pro_null.png'); ?>" alt="User Profile">
-                            <span><?php echo htmlspecialchars($_SESSION['first_name'] . ' ' . $_SESSION['last_name']); ?></span>
-                        </div>
-                        <a href="logout.php">Log Out</a>
-                        <a href="profile.php">Profile</a>
+                    <!-- Display user profile when logged in -->
+                    <div class="mobile_user_profile">
+                        <img src="<?php echo htmlspecialchars($_SESSION['profile_image'] ?? 'assets/images/profiles/pro_null.png'); ?>"
+                            alt="User Profile">
+                        <span><?php echo htmlspecialchars($_SESSION['first_name'] . ' ' . $_SESSION['last_name']); ?></span>
+                    </div>
+                    <a href="logout.php">Log Out</a>
+                    <a href="profile.php">Profile</a>
                     <?php else: ?>
-                        <!-- Display login and signup links when not logged in -->
-                        <a id="login-nav_m" href="#">log in</a>
-                        <a id="signup-nav_m" href="#">sign up</a>
+                    <!-- Display login and signup links when not logged in -->
+                    <a id="login-nav_m" href="#">log in</a>
+                    <a id="signup-nav_m" href="#">sign up</a>
                     <?php endif; ?>
                     <a href="add-place.php">add place</a>
                     <a href="index.php">home</a>
@@ -211,6 +223,5 @@ if (!isset($_SESSION['profile_image']) || !isset($_SESSION['first_name']) || !is
         </div>
     </nav>
 </body>
+
 </html>
-
-
