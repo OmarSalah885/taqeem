4f0146a64107630e056bc882ea23e919b54dfabc
OmarSalah885 <omarsalah00013@gmail.com>
2025-05-15 22:17:15 +0300

    delete unnessesary files

--- clear_session.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start(); // Start the session
-session_unset(); // Unset all session variables
-session_destroy(); // Destroy the session
-echo "Session data cleared. You can now test login and logout functionality.";
-?>
